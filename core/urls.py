b37fa10f7a53396d6afeb65ca9c4160d64a3e627
Artem <artem.vashenko201010@gmail.com>
2024-09-24 19:56:13 +0300

    Created user_app

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,16 @@
 from .settings import core_app
 
 import article_app
-
+import user_app
 
 article_app.article_app.add_url_rule(
     rule="/", 
     view_func= article_app.view_all_articles
     )
+user_app.user_app.add_url_rule(
+    rule="/User/",
+    view_func= user_app.render_template
+)
 
 core_app.register_blueprint(blueprint = article_app.article_app )
+core_app.register_blueprint(blueprint= user_app.user_app)
